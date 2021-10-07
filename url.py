2f8a52162ceedf30622882d6d5188a455aee61d9
coeus <sallahuddinkh@gmail.com>
2021-10-07 12:16:49 +0500

    Adding spacing to functions

--- url.py
+++ url.py
@@ -3,6 +3,7 @@ def remove_last_slash(url:str)->str:                    #Removes the last forwar
         return(url[:-1])
     else: 
         return url 
+
 def get_protocol(url:str)->str:                         #Gets the protocol specified in the url
     parsed_url = url.split('/')
     protocol = parsed_url[0].replace(":",'')
@@ -16,6 +17,7 @@ def get_path(url:str)->str:                             #Gets the server directo
     parsed_url = url.split('/',3)  
     path = parsed_url[3].split('?')
     return path[0]
+    
 def get_query_string_params(url:str)->dict:             #Returns the query arguments in form of a dictionary
     url = remove_last_slash(url)
     parsed_url = url.split('/',3)  
