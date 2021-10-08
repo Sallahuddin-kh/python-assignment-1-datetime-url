c341237bb675718d1d07d0de2b67c32c0c710a72
coeus <sallahuddinkh@gmail.com>
2021-10-08 15:17:25 +0500

    Fixing comment representation

--- datetimeutil.py
+++ datetimeutil.py
@@ -1,9 +1,9 @@
 from datetime import datetime as dt
 def get_components(datetime:str)->list:
-    '''
+    """
     get_components checks the length, format and values of date time. Returns the values
     as a list. If the pattern is not matched, return [0,0,0,0,0,0]
-    '''
+    """
     try:
         date_object = dt.strptime(datetime, "%d/%m/%Y %H:%M:%S")
         return_object = [date_object.year, date_object.month, date_object.day, date_object.hour, date_object.minute, date_object.second]
