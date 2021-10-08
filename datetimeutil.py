e2c18b037cbb524c364753ff828091c22b903aca
coeus <sallahuddinkh@gmail.com>
2021-10-08 16:19:11 +0500

    Code lining fixed and return statement fixed

--- datetimeutil.py
+++ datetimeutil.py
@@ -1,4 +1,5 @@
 from datetime import datetime as dt
+
 def get_components(datetime:str)->list:
     """
     get_components checks the length, format and values of date time. Returns the values
@@ -6,10 +7,7 @@ def get_components(datetime:str)->list:
     """
     try:
         date_object = dt.strptime(datetime, "%d/%m/%Y %H:%M:%S")
-        return_object = [date_object.year, date_object.month, date_object.day, date_object.hour, date_object.minute, date_object.second]
-        return return_object
+        return [date_object.year, date_object.month, date_object.day, date_object.hour, date_object.minute, date_object.second]
     except ValueError as ve:
         print(ve)
         return [0,0,0,0,0,0]
-
-
